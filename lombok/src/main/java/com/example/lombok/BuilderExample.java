550ceeed2e532933c56984bc454dc87359b75dcf
onozaty <onozaty@gmail.com>
2017-03-11 23:54:29 +0900

    Lombokを試すプロジェクト追加。Builderのサンプルコード追加。

---
+++ lombok/src/main/java/com/example/lombok/BuilderExample.java
@@ -0,0 +1,36 @@
+package com.example.lombok;
+
+import lombok.Builder;
+import lombok.Data;
+
+public class BuilderExample {
+
+    public static void main(String[] args) {
+
+        Customer customer1 = new Customer("Taro", "Urashima", "ura@exmple.com", "111-111-1111");
+
+        System.out.println(customer1);
+
+        Customer customer2 = Customer.builder()
+                .firstName("Taro")
+                .lastName("Urashima")
+                .mailAddress("ura@exmple.com")
+                .telephone("111-111-1111")
+                .build();
+
+        System.out.println(customer2);
+    }
+}
+
+@Data
+@Builder
+class Customer {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String mailAddress;
+
+    private String telephone;
+}
