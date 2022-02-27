884f3da701655d2e345ea286bf7cffb9548e4e68
onozaty <onozaty@gmail.com>
2022-02-27 16:35:14 +0900

    [Java8to17] Optionalクラスの拡張 の追加

--- java8to17/README.md
+++ java8to17/README.md
@@ -3,7 +3,7 @@
 * https://openjdk.java.net/projects/jdk9/
 * https://docs.oracle.com/javase/jp/9/whatsnew/toc.htm
 
-### JEP 269: Convenience Factory Methods for Collections
+### ★ JEP 269: Convenience Factory Methods for Collections
 
 コレクションクラスのファクトリメソッドが追加。  
 全てイミュータブル(追加/削除/変更不可)なコレクションになる。
@@ -31,7 +31,7 @@ list1.add("b");
 list1.add("c");
 list1 = Collections.unmodifiableList(list1);
 
-// Arrays#asList だと、要素の追加/削除はできないが、変更はできる
+// Arrays#asList だと、要素の追加/削除はできないが、変更はできてしまう
 List<String> list2 = Arrays.asList("a", "b", "c");
 list2.set(1, "bb");
 
@@ -48,7 +48,56 @@ map.put(1, "a");
 map.put(2, "b");
 map.put(3, "c");
 map = Collections.unmodifiableMap(map);
-```            
+```
+
+### ▲ Optionalクラスの拡張
+
+`ifPresent` は値があったときのみ実行されたが、値が無かった時のアクションも同時に実行できる`ifPresentOrElse`が追加された。
+
+```java
+AtomicInteger hasValueCounter = new AtomicInteger();
+AtomicInteger emptyValueCounter = new AtomicInteger();
+
+Optional<String> value = Optional.ofNullable(null);
+
+value.ifPresentOrElse(
+        v -> hasValueCounter.incrementAndGet(),
+        () -> emptyValueCounter.incrementAndGet());
+
+assertThat(hasValueCounter.get()).isEqualTo(0);
+assertThat(emptyValueCounter.get()).isEqualTo(1);
+```
+
+`orElse`と似たようなものとして、`or`が追加された。  
+`or` は`Optional`を返す`Supplier`を指定する。  
+`Optional`のままデフォルト値で補完するようなときに使えるのかもしれない。
+
+```java
+Optional<String> value = Optional.empty();
+
+Optional<String> result = value.or(() -> Optional.of(""));
+assertThat(result.get()).isEqualTo("");
+
+// 取り出す際に埋めるような場合には、orElseでも十分
+assertThat(value.orElse("")).isEqualTo("");
+```
+
+`stream`メソッドで`Stream`に変換できるようになった。  
+値がある場合、値が一つ格納された`Stream`、値が無い場合、空の`Stream`になる。
+
+```java
+Optional<String> value = Optional.of("a");
+
+long count = value.stream().count();
+assertThat(count).isEqualTo(1);
+```
+
+```java
+Optional<String> value = Optional.empty();
+
+long count = value.stream().count();
+assertThat(count).isEqualTo(0);
+```
 
 ## 参考
 
--- java8to17/src/test/java/com/github/onozaty/java8to17/Java9Test.java
+++ java8to17/src/test/java/com/github/onozaty/java8to17/Java9Test.java
@@ -1,12 +1,119 @@
 package com.github.onozaty.java8to17;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.assertj.core.data.MapEntry;
 import org.junit.Test;
 
 public class Java9Test {
 
     @Test
-    public void testJep269() {
-        new Java9().jep269();
+    public void collectionsFactory_jep269() {
+
+        // Java9 より前
+        {
+            // List
+            List<String> list1 = new ArrayList<>();
+            list1.add("a");
+            list1.add("b");
+            list1.add("c");
+            list1 = Collections.unmodifiableList(list1);
+
+            // Arrays#asList だと、要素の追加/削除はできないが、変更はできる
+            List<String> list2 = Arrays.asList("a", "b", "c");
+            list2.set(1, "bb");
+
+            // Set
+            Set<String> set = new HashSet<>();
+            set.add("a");
+            set.add("b");
+            set.add("c");
+            set = Collections.unmodifiableSet(set);
+
+            // Map
+            Map<Integer, String> map = new HashMap<>();
+            map.put(1, "a");
+            map.put(2, "b");
+            map.put(3, "c");
+            map = Collections.unmodifiableMap(map);
+        }
+
+        // Java9
+        {
+            // 全てイミュータブルになる(追加、削除、変更が不可)
+
+            // List
+            List<String> list = List.of("a", "b", "c");
+            assertThat(list).containsExactly("a", "b", "c");
+
+            // Set
+            Set<String> set = Set.of("a", "b", "c");
+            assertThat(set).containsExactly("a", "b", "c");
+
+            // Map
+            Map<Integer, String> map1 = Map.of(1, "a", 2, "b", 3, "c");
+            assertThat(map1).containsExactly(
+                    MapEntry.entry(1, "a"), MapEntry.entry(2, "b"), MapEntry.entry(3, "c"));
+
+            // Map#ofEntries を使った方が、キーと値のセットがわかりやすい
+            Map<Integer, String> map2 = Map.ofEntries(Map.entry(1, "a"), Map.entry(2, "b"), Map.entry(3, "c"));
+            assertThat(map2).containsExactly(
+                    MapEntry.entry(1, "a"), MapEntry.entry(2, "b"), MapEntry.entry(3, "c"));
+        }
+    }
+
+    @Test
+    public void optional() {
+
+        {
+            AtomicInteger hasValueCounter = new AtomicInteger();
+            AtomicInteger emptyValueCounter = new AtomicInteger();
+
+            Optional<String> value = Optional.ofNullable(null);
+
+            value.ifPresentOrElse(
+                    v -> hasValueCounter.incrementAndGet(),
+                    () -> emptyValueCounter.incrementAndGet());
+
+            assertThat(hasValueCounter.get()).isEqualTo(0);
+            assertThat(emptyValueCounter.get()).isEqualTo(1);
+        }
+
+        {
+            Optional<String> value = Optional.empty();
+
+            Optional<String> result = value.or(() -> Optional.of(""));
+
+            assertThat(result.get()).isEqualTo("");
+
+            // 取り出す際に埋めるような場合には、orElseでも十分
+            assertThat(value.orElse("")).isEqualTo("");
+        }
+
+        {
+            Optional<String> value = Optional.of("a");
+
+            long count = value.stream().count();
+            assertThat(count).isEqualTo(1);
+        }
+
+        {
+            Optional<String> value = Optional.empty();
+
+            long count = value.stream().count();
+            assertThat(count).isEqualTo(0);
+        }
     }
 
 }
