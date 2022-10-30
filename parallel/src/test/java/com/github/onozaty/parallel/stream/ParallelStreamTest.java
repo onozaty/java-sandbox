3c2e798399a748513ef183020a6fc978efc06d65
onozaty <onozaty@gmail.com>
2022-10-30 09:43:33 +0900

    [parallel] Parallel Streamの並列数を制限するコード追加

--- parallel/src/test/java/com/github/onozaty/parallel/stream/ParallelStreamTest.java
+++ parallel/src/test/java/com/github/onozaty/parallel/stream/ParallelStreamTest.java
@@ -2,6 +2,7 @@ package com.github.onozaty.parallel.stream;
 
 import java.io.IOException;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
@@ -12,12 +13,12 @@ import com.github.onozaty.parallel.Recorder;
 public class ParallelStreamTest {
 
     @Test
-    public void parallelCount() throws IOException {
+    public void parallelStream_default() throws IOException {
 
         Random random = new Random();
         Recorder recorder = new Recorder();
 
-        System.out.println("ForkJoinPool.getCommonPoolParallelism :" + ForkJoinPool.getCommonPoolParallelism());
+        System.out.println("ForkJoinPool.getCommonPoolParallelism :" + ForkJoinPool.commonPool().getParallelism());
 
         IntStream.range(0, 30).parallel()
                 .forEach(i -> {
@@ -35,6 +36,51 @@ public class ParallelStreamTest {
 
                     recorder.end(recordingId);
                 });
+
+        System.out.println();
+
+        System.out.println("By Redcording");
+        recorder.printByRecordingId(System.out);
+
+        System.out.println();
+
+        System.out.println("Active Count Graph");
+        recorder.printByActiveCount(System.out);
+    }
+
+    @Test
+    public void parallelStream_custom() throws IOException, InterruptedException, ExecutionException {
+
+        Random random = new Random();
+        Recorder recorder = new Recorder();
+
+        System.out.println("ForkJoinPool.getCommonPoolParallelism :" + ForkJoinPool.commonPool().getParallelism());
+
+        ForkJoinPool customThreadPool = new ForkJoinPool(3);
+        try {
+            customThreadPool.submit(
+                    () -> {
+                        IntStream.range(0, 30).parallel()
+                                .forEach(i -> {
+
+                                    System.out.println(Thread.currentThread().getName());
+
+                                    int recordingId = recorder.start();
+
+                                    try {
+                                        // 3秒内でランダムな時間待ち合わせ
+                                        Thread.sleep(random.nextInt(3000));
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    recorder.end(recordingId);
+                                });
+                    }).get();
+
+        } finally {
+            customThreadPool.shutdown();
+        }
 
         System.out.println();
 
